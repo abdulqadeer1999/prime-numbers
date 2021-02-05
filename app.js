0d761b44da8ca6ec682683241e13c874ef94034e
Abdul Qadeer Solangi <abdulqadeersolangi007@gmail.com>
2021-02-05 17:46:57 +0500

    prime number calculate

---
+++ app.js
@@ -0,0 +1,10 @@
+
+var num = prompt("Enter a number")
+
+for ( i= 0; i<=num.length; num++){
+    if (num %2 === 0){
+        alert( num + "isa prime number ")
+    }else {
+        alert("Not a prime no ")
+    }
+}
